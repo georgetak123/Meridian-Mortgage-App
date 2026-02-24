5ce4b99c9e1d09e902becc1c1260a3951e4bd361
georgetak123 <niamonitakisgeorge@gmail.com>
2026-02-23 21:14:59 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Meridian-Mortgage-App/',
+  build: {
+    sourcemap: false,
+    target: 'es2015',
+  }
 })
